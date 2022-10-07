cabead4ba57616998dd2e0c1114d5361235235e7
Nishchal Basyal <60071083+ainishchal@users.noreply.github.com>
2022-10-07 23:05:15 +0530

    Post Business Added

---
+++ app/src/main/java/com/example/classifiedappmain/blogsingleitem_layout.java
@@ -0,0 +1,14 @@
+package com.example.classifiedappmain;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class blogsingleitem_layout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_blogsingleitem_layout);
+    }
+}
